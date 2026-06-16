433bbe45b7c6e17de55d3f7a5c7079f3ab496e65
QnA Chinese <qnachinese@gmail.com>
2026-06-16 08:04:30 +0800

    QB: rebuild linguistics 2024/2025 official + detailed why-correct/why-wrong explanations (2025); exp multi-line; fix 4 empty slots

---
+++ deploy53.bat
@@ -0,0 +1,5 @@
+cd /d "%~dp0"
+git add -A
+git commit -m "QB: rebuild linguistics 2024/2025 official + detailed why-correct/why-wrong explanations (2025); exp multi-line; fix 4 empty slots"
+git push
+pause
